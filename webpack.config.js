c0e07fcd7e5a74e8b3931ed05d801cac446ab8ef
Nathan Jorde <nater_nater@me.com>
2017-03-19 18:39:52 -0500

    webpack environment changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,96 +4,120 @@ const webpack = require('webpack');
 const ExtractTextSass = new ExtractTextPlugin("index.one.[contenthash].css");
 const ExtractTextLess = new ExtractTextPlugin("index.two.[contenthash].css");
 
-const webpackConfig = {
-  entry: {
-    app: './src/entry',
-  },
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: __dirname + '/dist',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.scss'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'React-Redux-Weback-Typescript-AntDesign',
-      template: 'src/index.ejs'
-    }),
-    ExtractTextSass,
-    ExtractTextLess,
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.[chunkhash].js',
-      minChunks: function (module) {
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(tsx|ts)$/,
-        use: [
-          {
-            loader: 'awesome-typescript-loader',
-            options:  {
-              silent: true,
-              compilerOptions: { sourceMap: false },
-              useCache: true,
-              useBabel: true,
-              babelOptions: {
-                "presets": [
-                  "react",
-                  [ "es2015", { "modules": false } ],
-                ],
-                "plugins": [
-                  [ 
-                    "import", [
-                      { "libraryName": "antd", "libraryDirectory": "lib", "style": true },
-                      { "libraryName": "lodash" },
-                    ]
+const getConfig = (env) => {
+  let config = {
+    entry: {
+      app: './src/entry',
+    },
+    output: {
+      filename: '[name].[chunkhash].js',
+      path: __dirname + '/dist',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.scss'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'React-Redux-Weback-Typescript-AntDesign',
+        template: 'src/index.ejs'
+      }),
+      ExtractTextSass,
+      ExtractTextLess,
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts)$/,
+          use: [
+            {
+              loader: 'awesome-typescript-loader',
+              options:  {
+                silent: true,
+                compilerOptions: { sourceMap: false },
+                useCache: true,
+                useBabel: true,
+                babelOptions: {
+                  "presets": [
+                    "react",
+                    [ "es2015", { "modules": false } ],
                   ],
-                ],
+                  "plugins": [
+                    [ 
+                      "import", [
+                        { "libraryName": "antd", "libraryDirectory": "lib", "style": true },
+                        { "libraryName": "lodash" },
+                      ]
+                    ],
+                  ],
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.(css|scss)$/,
-        loader: ExtractTextSass.extract({
-          use: ['css-loader', 'sass-loader'],
-          fallback:'style-loader',
-        })
-      },
-      {
-        test: /\.(less)$/,
-        loader: ExtractTextLess.extract({
-          use: ['css-loader', 'less-loader'],
-          fallback:'style-loader',
-        })
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=fonts/[name].[hash].[ext]'
-      }
-    ],
-  },
-  devServer: {
-    proxy: {
-      '/api': {
-        target: {
-          host: 'localhost',
-          protocol: 'http',
-          port: 3000,
+          ],
         },
-        changeOrigin: true,
-        secure: false,
-      },
+        {
+          test: /\.(css|scss)$/,
+          loader: ExtractTextSass.extract({
+            use: ['css-loader', 'sass-loader'],
+            fallback:'style-loader',
+          })
+        },
+        {
+          test: /\.(less)$/,
+          loader: ExtractTextLess.extract({
+            use: ['css-loader', 'less-loader'],
+            fallback:'style-loader',
+          })
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          loader: 'file-loader?name=fonts/[name].[hash].[ext]'
+        }
+      ],
     },
-  },
+  };
+
+  if (env === 'dev') {
+    config.devServer = {
+      proxy: {
+        '/api': {
+          target: {
+            host: 'localhost',
+            protocol: 'http',
+            port: 3000,
+          },
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    }
+  }
+
+  if (env === 'prod') {
+    config.plugins.push(
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.[chunkhash].js',
+        minChunks: function (module) {
+          return module.context && module.context.indexOf('node_modules') !== -1;
+        }
+      })
+    );
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+          beautify: false,
+          mangle: {
+              screw_ie8: true,
+              keep_fnames: true
+          },
+          compress: {
+              screw_ie8: true
+          },
+          comments: false
+      })
+    )
+  }
+
+  return config;
 };
 
-module.exports = webpackConfig;
+module.exports = getConfig;
